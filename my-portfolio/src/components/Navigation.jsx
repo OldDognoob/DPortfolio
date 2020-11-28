da72e4640eeedf1eadaea37a6018a3d00b7c7ac2
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-28 18:40:33 +0200

    navigation

--- my-portfolio/src/components/Navigation.jsx
+++ my-portfolio/src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "bootstrap/dict/css/bootstrap.css";
 import { Navbar, Nav } from "react-bootstrap";
 import HomeIcon from "@material-ui/icons/Home";
 import { Link } from "react-scroll";
@@ -25,7 +26,29 @@ function Navigation() {
           to="landing"
           smooth={true}
           duration={1000}
-        ></Navbar.Brand>
+        >
+          <HomeIcon/>
+        </Navbar.Brand>
+        <Navbar.Toggle
+        onClick={handleClick}
+        ara-controls="responsive-navbar-nav"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as ={Link} to="about"smooth={true} duration={1000}>
+              About
+            </Nav.Link>
+            <Nav.Link as ={Link} to="about"smooth={true} duration={1500}>
+              Skills
+            </Nav.Link>
+            <Nav.Link as ={LinkRoute} to="/projects">
+              Projects
+            </Nav.Link>
+            <Nav.Link as ={Link} to="about"smooth={true} duration={2000}>
+              Contact
+            </Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
       </Navbar>
     </div>
   );
