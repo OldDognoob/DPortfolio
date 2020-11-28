2d7d79f957e9acec7a9ba47a0fb322afdec84d86
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-28 18:48:12 +0200

    reafactoring-app-file

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
 
-function App(){
+function App() {
   return (
     <div>
-      Hello People!
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/projects" exact component={Projects}/>
+        </Switch>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 export default App;
