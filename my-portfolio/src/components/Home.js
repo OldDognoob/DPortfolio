c4738d5563a53b6549ecf9b77cc9cec411e9c629
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-08 20:48:00 +0200

    delete-dark-fantionality

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -6,22 +6,11 @@ import MySkills from "./MySkills";
 import Contact from "./Contact";
 
 import Works from "./Works";
-import useDarkMode from "./useDarkMode";
-import Toggle from "./Toggle";
 
-// const location ={
-//   address:'8 Chrysochooy Street, Larisa, Greece',
-//   lat:39.643452,
-//   lng:22.413208
-// }
 
 function Home() {
-  const [theme, toggleTheme] = useDarkMode();
-  // console.log(theme);
-
   return (
     <div className="home__components">
-      <Toggle theme={theme} toggleTheme={toggleTheme}/>
       <Navigation />
       <Landing />
       <About />
