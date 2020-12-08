b810781225b7139c7601518a66a3165ac71b774d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-08 22:07:29 +0200

    refactoring

--- my-portfolio/src/components/useMedia.js
+++ my-portfolio/src/components/useMedia.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
 
 // A hook that accepts a single media query
 export default function useMedia(queries, values, defaultValue) {
@@ -7,14 +6,14 @@ export default function useMedia(queries, values, defaultValue) {
   const [value, setValue] = useState(defaultValue);
 
   // Array containing a media query list for each query
-  const mediaQueryLists = queries.map(q => window.matchMedia(q));
+  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
 
   // State update function
   const getValue = () => {
     // Get index of first media query that matches
-    const index = mediaQueryLists.findIndex(mql => mql.matches);
+    const index = mediaQueryLists.findIndex((mql) => mql.matches);
     // Return related value or defaultValue if none
-    return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
+    return typeof values[index] !== "undefined" ? values[index] : defaultValue;
   };
 
   useEffect(
@@ -22,16 +21,16 @@ export default function useMedia(queries, values, defaultValue) {
       // Set the initial value
       setValue(getValue);
 
-    
       // defining getValue outside of useEffect we ensure that it has ...
       // ... current values of hook args (as this hook only run on mount/dismount).
       const handler = () => setValue(getValue);
 
       // Set a listener for each media query with above handler as callback.
-      mediaQueryLists.forEach(mql => mql.addListener(handler));
+      mediaQueryLists.forEach((mql) => mql.addListener(handler));
 
       // Remove listeners on cleanup
-      return () => mediaQueryLists.forEach(mql => mql.removeListener(handler));
+      return () =>
+        mediaQueryLists.forEach((mql) => mql.removeListener(handler));
     },
     [] // we are ensuring that it would not be an infinity mounting
   );
