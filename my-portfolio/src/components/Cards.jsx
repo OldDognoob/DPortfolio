fb6e6c40852aeab1048ac3f12c21f25833974bee
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-14 21:45:40 +0200

    changes

--- my-portfolio/src/components/Cards.jsx
+++ my-portfolio/src/components/Cards.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CardItem from "./CardItem";
+import wunderlist from "../img/wunderlist.png";
+import africaMarket from "../img/africaMarket.png"
 import "./cards.css";
 
 function Cards() {
@@ -8,9 +10,27 @@ function Cards() {
       <div className="cards__container">
         <ul className="cards__items">
           <CardItem 
+          path="https://wunder-list.netlify.app/" 
+          category="HTML5, CSS3, LESS, JS"
+          imgSrc= {wunderlist}
+          imgAlt="wunderlist-img" 
+          title="Wunderlist 2.0" 
+          info="A web app list for users to track down their tasks they need to complete." 
+          />
+
+          <CardItem 
+          path="https://africa-market-place.netlify.app/" 
+          category="CSS3, REACT" 
+          imgSrc={africaMarket} 
+          imgAlt="africaMarket-img" 
+          title="Africa Market Place" 
+          info="A virtual market place for Africa business owners, where they can sell their products." 
+          />
+
+          <CardItem 
           path="" 
           category="" 
-          imgSrc={} 
+          // imgSrc={} 
           imgAlt="" 
           title="" 
           info="" 
@@ -19,25 +39,7 @@ function Cards() {
           <CardItem 
           path="" 
           category="" 
-          imgSrc={} 
-          imgAlt="" 
-          title="" 
-          info="" 
-          />
-
-          <CardItem 
-          path="" 
-          category="" 
-          imgSrc={} 
-          imgAlt="" 
-          title="" 
-          info="" 
-          />
-
-          <CardItem 
-          path="" 
-          category="" 
-          imgSrc={} 
+          // imgSrc={} 
           imgAlt="" 
           title="" 
           info="" 
